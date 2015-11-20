76d0d1d36185425a6226507c97ace33191705575
seishi <185766516@qq.com>
2015-11-20 14:55:40 +0800

    添加设备引导提示修改

--- src/nutri/views/guide.vue
+++ src/nutri/views/guide.vue
@@ -1,19 +1,15 @@
 <template lang="jade">
 .main-content
-  .welcome-words
-    | Hi~欢迎，在添加营养称前，
-    <br> 确保您是否已经进行以下两个步骤
+  .welcome
+    p Hi~欢迎，在添加营养称前，
+    p 确保您是否已经进行以下两个步骤
 
-  .switch
-    img.switch-bluetooth
-    .switch-tip 请打开手机蓝牙
+  .bluetooth-tip 请打开手机蓝牙
 
-  .switch
-    img.switch-nutri
-    .switch-tip 开启营养秤开关
+  .scale-tip 开启营养秤开关
 
-  .actions
-    button(type="button") 下一步
+  .foot-actions
+    button.btn-block(type="button") 下一步
 
   //- modal
   //-   .modal-footer
@@ -24,19 +20,12 @@
 <style lang="stylus">
 @import '../../shared/assets/stylus/common'
 /* 欢迎词 */
-.welcome-words
+.welcome
   font-size rem(16)
   text-align center
 
-/* 开关 */
-.switch
-  width 100%
-  text-align center
-
-  img
-    size 80% rem(200)
-
-.switch-tip
+.bluetooth-tip
+.scale-tip
   font-size rem(16)
   text-align center
 
