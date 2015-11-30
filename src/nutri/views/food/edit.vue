6fb2dfc15132bfb0f8ab2b8a7b0b70027b2c2004
chanyan <yan_chan17@163.com>
2015-11-30 15:20:20 +0800

    食材编辑

--- src/nutri/views/food/edit.vue
+++ src/nutri/views/food/edit.vue
@@ -2,85 +2,147 @@
   .main-content
     .food-edit-list
       form(v-form, name="foodEditForm", @submit="onSubmit")
-        ul
+        ul.edit-required-form
           li
-            span 名称
-            input(v-model="food.name", v-form-ctrl, required, name="name", @focus="editingName=true", @blur="editingName=false")
-            button(v-show="!isEmpty(food.name) && editingName", @mousedown="food.name=''") X
-      //- form(v-form, name="foodEditForm", @submit="onSubmit")
-      //- ul
-      //-   li
-      //-     span 名称
-      //-     input(v-model="food.name", v-form-ctrl, required, name="name", @focus="editingName=true", @blur="editingName=false")
-      //-     button(v-show="!isEmpty(food.name) && editingName", @mousedown="food.name=''") X
-        //- li
-        //-   span 重量
-        //-   input(v-model="weight", @focus="editingWeight=true", @blur="editingWeight=false")
-        //-   button(v-show="!isEmpty(weight) && editingWeight", @mousedown="weight=''") X
-        //- li
-        //-   span 热量
-        //-   input(v-model="heat", @focus="editingHeat=true", @blur="editingHeat=false")
-        //-   button(v-show="!isEmpty(heat) && editingHeat", @mousedown="heat=''") X
-        //- li
-        //-   span 蛋白质
-        //-   input(v-model="protein", @focus="editingProtein=true", @blur="editingProtein=false")
-        //-   button(v-show="!isEmpty(protein) && editingProtein", @mousedown="protein=''") X
-        //- li
-        //-   span 脂肪
-        //-   input(v-model="fat", @focus="editingFat=true", @blur="editingFat=false")
-        //-   button(v-show="!isEmpty(fat) && editingFat", @mousedown="fat=''") X
-        //- li
-        //-   span 胆固醇
-        //-   input(v-model="chol", @focus="editingChol=true", @blur="editingChol=false")
-        //-   button(v-show="!isEmpty(chol) && editingChol", @mousedown="chol=''") X
-        //- li
-        //-   span 糖类
-        //-   input(v-model="sugar", @focus="editingSugar=true", @blur="editingSugar=false")
-        //-   button(v-show="!isEmpty(sugar) && editingSugar", @mousedown="sugar=''") X
-        //- li
-        //-   span 纤维素
-        //-   input(v-model="fibrin", @focus="editingFibrin=true", @blur="editingFibrin=false")
-        //-   button(v-show="!isEmpty(fibrin) && editingFibrin", @mousedown="fibrin=''") X
-        //- li
-        //-   span 钠
-        //-   input(v-model="natrium", @focus="editingNatrium=true", @blur="editingNatrium=false")
-        //-   button(v-show="!isEmpty(natrium) && editingNatrium", @mousedown="natrium=''") X
-    //- .errors(v-if="foodEditForm.$submitted")
-    //-   p(v-if="foodEditForm.name.$error.required") 请输入用户名
-    //- .foot-actions
-    //-   button.btn-block(type="button") 保存
+            span Name
+            div
+              input(v-model="food.name", v-form-ctrl, required, name="food.name", @focus="editingName=true", @blur="editingName=false")
+              button.btn-delete(v-show="!isEmpty(food.name) && editingName", @mousedown="food.name=''")
+          li
+            span Weight
+            div
+              input(type="number", v-model="food.weight", , v-form-ctrl, required, name="food.weight" @focus="editingWeight=true", @blur="editingWeight=false")
+              button.btn-delete(v-show="!isEmpty(food.weight) && editingWeight", @mousedown="food.weight=''")
+        ul.edit-optional-form
+          li
+            span Kcal
+            div
+              input(type="number", v-model="food.heat", @focus="editingHeat=true", @blur="editingHeat=false")
+              button.btn-delete(v-show="!isEmpty(food.heat) && editingHeat", @mousedown="food.heat=''")
+          li
+            span Protein
+            div
+              input(type="number", v-model="food.protein", @focus="editingProtein=true", @blur="editingProtein=false")
+              button.btn-delete(v-show="!isEmpty(food.protein) && editingProtein", @mousedown="food.protein=''")
+          li
+            span Body Fat
+            div
+              input(type="number", v-model="food.fat", @focus="editingFat=true", @blur="editingFat=false")
+              button.btn-delete(v-show="!isEmpty(food.fat) && editingFat", @mousedown="food.fat=''")
+          li
+            span Cholesterol
+            div
+              input(type="number", v-model="food.chol", @focus="editingChol=true", @blur="editingChol=false")
+              button.btn-delete(v-show="!isEmpty(food.chol) && editingChol", @mousedown="food.chol=''")
+          li
+            span Sugar
+            div
+              input(type="number", v-model="food.sugar", @focus="editingSugar=true", @blur="editingSugar=false")
+              button.btn-delete(v-show="!isEmpty(food.sugar) && editingSugar", @mousedown="food.sugar=''")
+          li
+            span Fiber
+            div
+              input(type="number", v-model="food.fibrin", @focus="editingFibrin=true", @blur="editingFibrin=false")
+              button.btn-delete(v-show="!isEmpty(food.fibrin) && editingFibrin", @mousedown="food.fibrin=''")
+          li
+            span Sodium
+            div
+              input(type="number", v-model="food.natrium", @focus="editingNatrium=true", @blur="editingNatrium=false")
+              button.btn-delete(v-show="!isEmpty(food.natrium) && editingNatrium", @mousedown="food.natrium=''")
+    .foot-actions
+      button.btn.btn-primary(type="button") 保存
 </template>
 
 <style lang="stylus">
   @import '../../../shared/assets/stylus/common'
 
-  /* 个人食材列表 */
   .food-edit-list
+    padding-top rem(24)
+
+  /* 必填表单 */
+  .edit-required-form
+    width 100%
+    border-bottom rem(4) solid #FFF
+
+    li
+      position relative
+      width 100%
+      height rem(104)
+      clearfix()
+
+      span
+        float left
+        width 20%
+        margin-left 5%
+        height rem(104)
+        line-height rem(104)
+        font-size rem(30)
+        color #FFF
+
+      div
+        float left
+        width 70%
+        height rem(104)
+        padding-right 5%
+        border-top 1px solid #FFF
+
+        input
+          width 80%
+          height rem(104)
+          border none
+          outline none
+          padding 0
+          background transparent
+          color #FFF
+
+  /* 选填表单 */
+  .edit-optional-form
     width 100%
 
     li
-      width 90%
-      height rem(100)
-      line-height rem(100)
-      padding 0 5%
-      border-bottom 1px solid #666
+      position relative
+      width 100%
+      height rem(104)
+      clearfix()
 
       span
-        display inline-block
-        width rem(150)
+        float left
+        width 30%
+        margin-left 5%
+        margin-right 5%
+        height rem(104)
+        line-height rem(104)
+        font-size rem(30)
+        color #FFF
+        text-align right
 
-      input
-        width rem(300)
-        height rem(83)
-        border none
-        padding 0
-        outline none
+      div
+        float left
+        width 55%
+        height rem(104)
+        padding-right 5%
+        border-bottom 1px solid #FFF
 
-      button
-        size rem(100)
-        border none
-        background none
-        padding 0
+        input
+          width 80%
+          height rem(104)
+          border none
+          outline none
+          padding 0
+          background transparent
+          color #FFF
+
+  /* 删除按钮 */
+  .btn-delete
+    absolute right 8% top
+    size rem(30) rem(104)
+    border none
+    background none
+    padding 0
+    background url('../../../shared/assets/images/bg/icon_delete.png') no-repeat 0 center
+    background-size rem(30) rem(30)
+
+
 </style>
 
 <script>
@@ -93,7 +155,7 @@ var locales = require('../../../shared/consts/locales');
 module.exports = {
   data: function () {
     return {
-/*    editingName: false,
+      editingName: false,
       editingWeight: false,
       editingHeat: false,
       editingProtein: false,
@@ -101,7 +163,7 @@ module.exports = {
       editingChol: false,
       editingSugar: false,
       editingFibrin: false,
-      editingNatrium: false,*/
+      editingNatrium: false,
       food: {
         name: '',
         weight: 0,
